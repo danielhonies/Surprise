f23bc77246da89a4b082ec4642afe4675761cba7
Daniel Honies <daniel.honies@gmail.com>
2016-04-26 10:50:58 +0200

    Initial Commit

---
+++ Surprise/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Surprise
+//
+//  Created by Daniel Honies on 26.04.16.
+//  Copyright © 2016 Daniel Honies. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
